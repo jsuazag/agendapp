32b3e465511531af7735cda608beba89247ea350
Jerson Suaza <jerson.suaza@gmail.com>
2021-04-13 21:06:34 -0500

    field component add

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
 import { InputWrapper, Label, InputType } from "./styles";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Icono = ({ showEye, onPress }) => (
+  <span onClick={onPress}>{showEye ? <FaEyeSlash /> : <FaEye />}</span>
+);
 
 export const Input = ({ label, type, placeholder }) => {
+  const [typeInput, setTypeInput] = useState(type);
+
+  const toogleTypeInput = (e) => {
+    setTypeInput(typeInput === "password" ? "text" : "password");
+  };
+
   return (
     <section>
       <Label>{label}</Label>
       <InputWrapper>
-        <InputType placeholder={placeholder} type={type} />
-        {type === "password" && <i>icono</i>}
+        <InputType placeholder={placeholder} type={typeInput} />
+        {type === "password" && (
+          <Icono onPress={toogleTypeInput} showEye={typeInput === "text"} />
+        )}
       </InputWrapper>
     </section>
   );
