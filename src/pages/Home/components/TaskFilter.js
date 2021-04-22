fc38fca2761d13d0dcae87da46378949ce5b4968
Jerson Suaza <jerson.suaza@gmail.com>
2021-04-21 19:34:58 -0500

    filter component style

--- src/pages/Home/components/TaskFilter.js
+++ src/pages/Home/components/TaskFilter.js
@@ -1,13 +1,17 @@
+import { TASK_STATUS } from "../../../constants/TaskStatus";
 import { TaskFilterWrapper, FilterStatusTaskWrapper } from "./styles";
 
-const FilterStatusTask = ({statusName}) => (
-    <FilterStatusTaskWrapper>
-        {statusName}
-    </FilterStatusTaskWrapper>
-)
+const FilterStatusTask = ({ statusName }) => (
+  <FilterStatusTaskWrapper>{statusName}</FilterStatusTaskWrapper>
+);
 
 export const TaskFilter = () => (
-    <TaskFilterWrapper>
-        <FilterStatusTask statusName="All" />
-    </TaskFilterWrapper>
-)
+  <TaskFilterWrapper>
+    <FilterStatusTask statusName="All" />
+    {
+        TASK_STATUS.map((item, key) => (
+            <FilterStatusTask key={key} statusName={item.name}  /> 
+        ))
+    }
+  </TaskFilterWrapper>
+);
--- src/pages/Home/components/styles.js
+++ src/pages/Home/components/styles.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const TaskFilterWrapper = styled.div`
-    border: 1px solid red;
     display: flex;
+    justify-content: space-between;
+    margin: 30px 0px 20px;
 `;
 
 export const FilterStatusTaskWrapper = styled.div`
-
+    font-size: 1.1em;
+    color: ${props => props.active ? '#0066FF' : '#777'};
+    font-weight: ${props => props.active ? 'bold' : '300'};
+    cursor: pointer;
+    &:hover {
+        color: #0066FF;
+    }
 `;
