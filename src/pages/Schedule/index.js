75f21483cdef1600f4ae1e2b9a336cc60f9740d4
Jerson Suaza <jerson.suaza@gmail.com>
2021-05-04 20:57:21 -0500

    calendar component add

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -1,9 +1,51 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Topbar } from "../../components/Topbar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
 
-export const Schedule = ({title}) => (
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Tarea 1",
+    start: new Date(),
+    end: new Date(),
+  },
+];
+
+const CALENDAR_VIEW_MODE = {
+  DAY: 'day',
+  WEEK: 'week'
+}
+
+export const Schedule = ({ title }) => {
+  const [calendarDefaultView, setCalendarDefaultView] = useState(CALENDAR_VIEW_MODE.DAY);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const desktop = window.innerWidth > 768;
+    setCalendarDefaultView(desktop ? CALENDAR_VIEW_MODE.WEEK : CALENDAR_VIEW_MODE.DAY);
+    setLoading(false);
+  }, []);
+
+  return (
     <Fragment>
-        <Topbar title={title} />
-        <p>Schedule</p>
+      <Topbar title={title} />
+      {
+        !loading && (
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: "70vh" }}
+            defaultView={calendarDefaultView}
+            views={["month", "week", "day"]}
+          />
+        )
+      }
+      
     </Fragment>
-)
+  );
+};
