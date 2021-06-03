429da27b9401942943e5127f7cc471e111a34913
Jerson Suaza <jerson.suaza@gmail.com>
2021-06-02 19:12:16 -0500

    http util add

--- src/store/tasks/taskActions.js
+++ src/store/tasks/taskActions.js
@@ -3,26 +3,28 @@ import {
   FETCH_TASKS_REQUEST,
   FETCH_TASKS_SUCCESS,
 } from "./taskTypes";
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchTasks = (status = 0) => {
   return (dispacth) => {
-      dispacth(fetchTaskRequest());
-      const url = 'http://localhost:4000/tasks';
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFVzZXIiOiI2MGIwMmQ1M2M5NjJkZTNiZTg5YTkyNDIiLCJyb2xlIjoxLCJpYXQiOjE2MjI2NzU4OTksImV4cCI6MTYyMjcxOTA5OX0.cVv7wK9yK2zkiCpWYs-CCDR1e28QBrpLTYcQSQ-VK-U";
-      const headers = {
-        'Authorization': `Bearer ${token}`
-      }
-      axios.get(url, { headers } )
-      .then(response => {
+    dispacth(fetchTaskRequest());
+    const url = "http://localhost:4000/tasks";
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFVzZXIiOiI2MGIwMmQ1M2M5NjJkZTNiZTg5YTkyNDIiLCJyb2xlIjoxLCJpYXQiOjE2MjI2NzU4OTksImV4cCI6MTYyMjcxOTA5OX0.cVv7wK9yK2zkiCpWYs-CCDR1e28QBrpLTYcQSQ-VK-U";
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    axios
+      .get(url, { headers })
+      .then((response) => {
         setTimeout(() => {
           dispacth(fetchTaskSuccess(response.data));
         }, 2000);
       })
-      .catch(error => {
+      .catch((error) => {
         dispacth(fetchTaskFailure(error.response.statusText));
       });
-  }
+  };
 };
 
 export const fetchTaskRequest = () => {
---
+++ src/utils/HttpRequest.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export const HTTP_VERBS = {
+  POST: "post",
+  GET: "get",
+  DELETE: "delete",
+  PUT: "put",
+};
+
+const headersConfig = (token) => {
+  return {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+};
+
+export const requestHttp = async ({
+  method = HTTP_VERBS.POST,
+  endpoint,
+  data = {},
+  params = {},
+  token = "",
+}) => {
+  try {
+    const url = process.env.REACT_APP_BASE_URL + endpoint;
+    const options = {
+      method,
+      url,
+      data,
+      params,
+      headers: headersConfig(token),
+    };
+    return await axios(options);
+  } catch (error) {
+      throw error;
+  }
+};
